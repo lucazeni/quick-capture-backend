27da9860daca1664f632d6d95122a60eb2b75b88
Luca Zeni <luca_zeni@hotmail.com>
2020-05-29 21:51:19 -0400

    initial commit

---
+++ room.ts
@@ -0,0 +1,50 @@
+import { Player } from "./player";
+
+export type TeamType = 'TEAMA' | 'TEAMB' | 'UNASSIGNED';
+
+export class Room {
+    _id: number;
+    _roomName: string;
+    _host: Player;
+    _players: Player[];
+    _teamA: Player[];
+    _teamB: Player[];
+    _chat: String[];
+    _roomSize: number;
+    _roomTimeLimit: Date;
+
+    constructor(roomName, host) {
+        this._id = Math.random() * 10E16;
+        this._roomName = roomName;
+        this._host = host;
+        this._roomTimeLimit = new Date();
+    }
+
+    getRoomName(): string {
+        return this._roomName;
+    }
+
+    setRoomName(roomName: string) {
+        this._roomName = roomName;
+    }
+
+    getHost(): Player {
+        return this._host;
+    }
+
+    setHost(host: Player) {
+        this._host = host;
+    }
+
+    getPlayers(): Player[] {
+        return this._players;
+    }
+
+    addPlayer(player: Player) {
+        this._players.push(player);
+        this._roomSize++;
+    }
+
+
+
+}
